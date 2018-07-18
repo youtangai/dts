081915d9210c0950eaf1dd29b0c6b08890f60bfe
youtangai <youtanagai@gmail.com>
2018-07-18 23:40:17 +0900

    [mod]send file data

--- cr_server/main.go
+++ cr_server/main.go
@@ -7,15 +7,37 @@ import (
 	"log"
 	"google.golang.org/grpc"
 	"flag"
+	"io/ioutil"
+	//"path/filepath"
+	"os"
+	"io"
 )
 
-func runGetFileInfo(client pb.FileTransferServiceClient) error {
+var (
+	client pb.FileTransferServiceClient
+)
+
+func runGetFolderInfo(folderName string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	folderInfo := &pb.FolderInfo{
+		Name: folderName,
+	}
+	res, err := client.GetFolderInfo(ctx, folderInfo)
+	if err != nil {
+		return err
+	}
+	log.Printf("return message: %s\n", res.Message)
+	return nil
+}
+
+func runGetFileInfo(name string, size int64, mode uint32) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	fileInfo := &pb.FileInfo{
-		Name: "name",
-		Size: 128,
-		Mode: 755,
+		Name: name,
+		Size: size,
+		Mode: mode,
 	}
 	res, err := client.GetFileInfo(ctx, fileInfo)
 	if err != nil {
@@ -25,13 +47,14 @@ func runGetFileInfo(client pb.FileTransferServiceClient) error {
 	return nil
 }
 
-func runTransferFile(client pb.FileTransferServiceClient) error {
+func runTransferFile(data []byte) error {
+	log.Println(string(data))
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	stream, err := client.TransferFile(ctx)
 	for {
 		fileData := &pb.FileData {
-			Data: []byte("hogehoge"),
+			Data: data,
 		}
 		if err := stream.Send(fileData);  err != nil {
 			return err
@@ -50,16 +73,44 @@ func runTransferFile(client pb.FileTransferServiceClient) error {
 func main() {
 	host := flag.String("host", "localhost", "server host name or ip addr")
 	port := flag.String("port", "12345", "server port number")
+	dir := flag.String("dir", ".", "transfer dir")
 	flag.Parse()
 	log.Printf("request to %s:%s", *host, *port)
+
 	conn, err := grpc.Dial(*host + ":" + *port, grpc.WithInsecure())
 	if err != nil {
-		log.Fatal("failed to dial: %v", err)
+		log.Fatalf("failed to dial: %v", err)
 	}
 	defer conn.Close()
 
-	client := pb.NewFileTransferServiceClient(conn)
+	client = pb.NewFileTransferServiceClient(conn)
 
-	runGetFileInfo(client)
-	runTransferFile(client)
+	runGetFolderInfo(*dir)
+	
+	files, err := ioutil.ReadDir(*dir)
+	if err != nil {
+		log.Fatalf("failed to read dir: %+V\n", err)
+	}
+	for _, file := range files {
+		runGetFileInfo(file.Name(), file.Size(), uint32(file.Mode()))
+		err = os.Chdir(*dir)
+		if err != nil {
+			log.Fatalf("failed to change dir: %v\n", err)
+		}
+		f, err := os.Open(file.Name())
+		if err != nil {
+			log.Fatalf("cannot open file: %s err: %v\n", file.Name(), err)
+		}
+		buff := make([]byte, file.Size())
+		for {
+			count, err := f.Read(buff)
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("failed to read: %v\n", err)
+			}
+			runTransferFile(buff[:count])
+		}
+	}
 }
