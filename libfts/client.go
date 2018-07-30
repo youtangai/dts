5d28aa0601b6dd2be1aaac4d47189b12f3b910d9
youtangai <youtanagai@gmail.com>
2018-07-30 19:34:01 +0900

    [fix]パッケージとして利用できるように改造

--- libfts/client.go
+++ libfts/client.go
@@ -1,8 +1,7 @@
-package main
+package libfts
 
 import (
-	"flag"
-	pb "github.com/youtangai/zanshin/proto"
+	pb "github.com/youtangai/fts/proto"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"io"
@@ -10,23 +9,27 @@ import (
 	"log"
 	"os"
 	"time"
+	"github.com/urfave/cli"
+	"path/filepath"
 )
 
-var (
+//Client is file transfer service's client
+type Client struct {
 	client pb.FileTransferServiceClient
-)
+}
 
 const (
-	MAX_BUFF = 16384
+	//MaxBuff is max buffer file transfer 
+	MaxBuff = 16384
 )
 
-func runGetFolderInfo(folderName string) error {
+func (c *Client)runGetFolderInfo(folderName string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	folderInfo := &pb.FolderInfo{
 		Name: folderName,
 	}
-	res, err := client.GetFolderInfo(ctx, folderInfo)
+	res, err := c.client.GetFolderInfo(ctx, folderInfo)
 	if err != nil {
 		return err
 	}
@@ -34,14 +37,14 @@ func runGetFolderInfo(folderName string) error {
 	return nil
 }
 
-func runGetFileInfo(name string, size int64, mode uint32) error {
+func (c *Client)runGetFileInfo(name string, size int64, mode uint32) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	fileInfo := &pb.FileInfo{
 		Name: name,
 		Mode: mode,
 	}
-	res, err := client.GetFileInfo(ctx, fileInfo)
+	res, err := c.client.GetFileInfo(ctx, fileInfo)
 	if err != nil {
 		return err
 	}
@@ -49,15 +52,12 @@ func runGetFileInfo(name string, size int64, mode uint32) error {
 	return nil
 }
 
-func runTransferFile(file *os.File) error {
+func (c *Client)runTransferFile(file *os.File) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	stream, err := client.TransferFile(ctx)
-	buff := make([]byte, MAX_BUFF)
-	n := 0
+	stream, err := c.client.TransferFile(ctx)
+	buff := make([]byte, MaxBuff)
 	for {
-		log.Println("n:", n)
-		n++
 		count, err := file.Read(buff)
 		if err == io.EOF {
 			break
@@ -81,45 +81,48 @@ func runTransferFile(file *os.File) error {
 	return nil
 }
 
-func main() {
-	host := flag.String("host", "localhost", "server host name or ip addr")
-	port := flag.String("port", "12345", "server port number")
-	dir := flag.String("dir", "", "transfer dir")
-	containerId := flag.String("c", "", "container id")
-	flag.Parse()
 
-	//todo: container checkpoint process
+//TransferDir
+func TransferDir(context *cli.Context) {
+	host := context.String("host")
+	port := context.String("port")
+	dir := context.Args().Get(0)
+    path, err := filepath.Abs(dir)
+	if err != nil {
+		log.Fatal("failed to get abs path", path)
+	}
 
-	log.Printf("container id : %s\n", *containerId)
+	log.Println("request to", host, ":",  port)
+	log.Println("dir:", dir, "path:", path)
 
-	log.Printf("request to %s:%s", *host, *port)
-
-	conn, err := grpc.Dial(*host+":"+*port, grpc.WithInsecure())
+	conn, err := grpc.Dial(host+":"+port, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("failed to dial: %v", err)
 	}
 	defer conn.Close()
 
-	client = pb.NewFileTransferServiceClient(conn)
+	ftsCli := &Client{
+		client: pb.NewFileTransferServiceClient(conn),
+	}
+	
+	ftsCli.runGetFolderInfo(dir)
 
-	runGetFolderInfo(*dir)
-
-	files, err := ioutil.ReadDir(*dir)
+	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		log.Fatalf("failed to read dir: %+v\n", err)
 	}
 
-	err = os.Chdir(*dir)
+	err = os.Chdir(dir)
 	if err != nil {
 		log.Fatalf("failed to change dir: %v\n", err)
 	}
 
 	for _, file := range files {
-		runGetFileInfo(file.Name(), file.Size(), uint32(file.Mode()))
+		ftsCli.runGetFileInfo(file.Name(), file.Size(), uint32(file.Mode()))
 		f, err := os.Open(file.Name())
 		if err != nil {
 			log.Fatalf("cannot open file: %s err: %v\n", file.Name(), err)
 		}
-		runTransferFile(f)
+		ftsCli.runTransferFile(f)
 	}
 }
